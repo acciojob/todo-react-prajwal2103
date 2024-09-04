74a42c717eac1d343af470f8c54e4376c5d7824e
prajwal2103 <prajwaljagtap5@gmail.com>
2024-09-04 23:15:21 +0530

    Submit6

--- src/components/App.js
+++ src/components/App.js
@@ -1,47 +1,47 @@
-
 import React from "react";
 import './../styles/App.css';
-import {useState} from 'react'
+import { useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
+
 const App = () => {
-  const[inputValue,setValue]=useState('');
-  const[task,setTask]=useState([]);
+  const [inputValue, setValue] = useState('');
+  const [task, setTask] = useState([]);
+
   return (
     <div id="main">
       <h1>To-Do List</h1>
-      <input type='text' value={inputValue} onChange={(event)=>
-        setValue(event.target.value)
-      } />
-      <button onClick={()=>{
-        let newTodo={
-          id:uuidv4(),
-          name:task
-        }
-        setTask([...task,newTodo])
-        setValue('')
-
-      }}>Add Todo</button>
+      <input
+        type='text'
+        value={inputValue}
+        onChange={(event) => setValue(event.target.value)}
+      />
+      <button onClick={() => {
+        let newTodo = {
+          id: uuidv4(),
+          name: inputValue
+        };
+        setTask([...task, newTodo]);
+        setValue('');
+      }}>
+        Add Todo
+      </button>
       <ul>
-        {
-          task.map((item)=>{
+        {task.map((item) => (
           <li key={item.id}>
             <div>{item.name}</div>
-            <button onClick={()=>{
-              let newArr=task.filter((element)=>{
-                return element.id !== item.id
-              })
-              setTask(newArr)
-              
-            }}>Delete</button>
+            <button onClick={() => {
+              let newArr = task.filter((element) => {
+                return element.id !== item.id;
+              });
+              setTask(newArr);
+            }}>
+              Delete
+            </button>
           </li>
-
-          })
-        }
+        ))}
       </ul>
-
-
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
